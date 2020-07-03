8ec0ad27b262994b7cb21de35e7b9611bcd41051
Caio Geraldes <caioaguida@gmail.com>
2020-07-03 16:30:44 -0300

    First edition, including Armenian, Avestan (trans and script), Glagolitic, Oscan, PIE and Vedic HK

---
+++ ie-maps/oscan.el
@@ -0,0 +1,43 @@
+;;;
+;;; Oscan: A Oscan transliteration scheme for emacs.
+;;;
+;;; AUTHOR: Caio Geraldes
+;;; EMAIL: caioaguida@protonmail.com
+;;; TWITTER: @silenus32
+;;;
+
+(require 'quail)
+
+(quail-define-package
+ "oscan" "Oscan" "𐌀𐌚" nil
+ "A transliteration scheme for Oscan characters."
+ nil t t t t nil nil nil nil nil t)
+
+(quail-define-rules
+  ("a" "𐌀")
+  ("b" "𐌁")
+  ("g" "𐌂")
+  ("d" "𐌃")
+  ("e" "𐌄")
+  ("v" "𐌅")
+  ("z" "𐌆")
+  ("h" "𐌇")
+  ("i" "𐌉")
+  ("l" "𐌋")
+  ("m" "𐌌")
+  ("n" "𐌍")
+  ("p" "𐌐")
+  ("ś" "𐌑")
+  ("sh" "𐌑")
+  ("r" "𐌓")
+  ("s" "𐌔")
+  ("t" "𐌕")
+  ("u" "𐌖")
+  ("f" "𐌚")
+  ("ú" "𐌞")
+  ("u/" "𐌞")
+  ("í" "𐌝")
+  ("i/" "𐌝")
+)
+
+(provide 'oscan)
---
+++ ie-maps/vedic-hk.el
@@ -0,0 +1,43 @@
+;;; ~/.doom.d/leim/quail/vedic-hk.el -*- lexical-binding: t; -*-
+
+(require 'quail)
+
+(quail-define-package
+ "vedic-hk" "Vedic - Harvard-Kyoto" "" nil
+ "A transliteration scheme for Vedic Sanskrit from HK to IAST."
+ nil t t t t nil nil nil nil nil t)
+
+(quail-define-rules
+ ("a/" "á")
+ ("i/" "í")
+ ("u/" "ú")
+ ("e/" "é")
+ ("o/" "ó")
+ ("A" "ā")
+ ("I" "ī")
+ ("U" "ū")
+ ("R" "r̥")
+ ("RR" "r̥̄")
+ ("lR" "l̥")
+ ("lRR" "l"̥̄)
+ ("A/" "ā́")
+ ("I/" "ī́")
+ ("U/" "ū́")
+ ("R/" "ŕ̥")
+ ("RR/" "r̥̄́")
+ ("lR/" "ĺ̥" )
+ ("lRR/" "l"̥̄́)
+ ("T" "ṭ")
+ ("D" "ḍ")
+ ("N" "ṇ")
+ ("S" "ṣ")
+ ("z" "ś")
+ ("L" "ḷ")
+ ("H" "ḥ")
+ ("M" "ṃ")
+ ("G" "ṅ")
+ ("J" "ñ")
+ ("&" "m̐")
+ )
+
+(provide 'vedic-hk)
