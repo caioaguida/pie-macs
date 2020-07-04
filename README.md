9afcf1821ebff562533fada68b056d6da44d9a9e
Caio Geraldes <caioaguida@gmail.com>
2020-07-03 22:01:42 -0300

    Some polishing

--- README.md
+++ README.md
@@ -3,12 +3,14 @@
 Some rather basic mappings for typing Indo-European languages and PIE reconstructions on Emacs.
 Most of the modes written so far were specially designed to work in a QWERTY keyboard.
 
-If you would like to use them, adding this to your config file will do the trick (don't forget to change the path to the files, according to your set up):
+Change the */path/path* bellow to the path where you installed the *pie-macs/* (do not include *pie-macs/* to your path).
 
 
 ```{elisp}
 ;; PIE-macs input methods
-(add-to-list 'load-path "/path/path/path/pie-macs/")
+(let ((default-directory  "/path/path/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
 (require 'pie-macs)
 ```
 
--- pie-macs.el
+++ pie-macs.el
@@ -1,12 +1,29 @@
-(add-to-list 'load-path "~/.doom.d/lisp/pie-macs/leim/quail")
-(require 'armenian)
-(require 'vedic-hk)
-(require 'pie)
-(require 'glagolitic)
-(require 'avestan-trans)
-(require 'avestan-script)
-(require 'old-persian)
-(require 'oscan)
-(require 'gothic)
+;;;
+;;; PIE-MACS: A (Proto-)Indo-European colection of transliteration schemes for emacs.
+;;;
+;;; AUTHOR: Caio Aguida
+;;; EMAIL: caioaguida@protonmail.com
+;;; TWITTER: @silenus32
+;;;
+
+;;
+;; Setting all the available mappings. If you would like to make this lighter, you could comment
+;; the lines for unwanted mappings.
+;;
+
+;; Rollback in case I decide to use (add-to-list 'load-path "path/path/pie-macs/") in the config file, not sure yet.
+;;
+;; (add-to-list 'load-path "~/.doom.d/lisp/pie-macs/leim/quail")
+;;
+
+(require 'armenian) ; Armenian Script
+(require 'vedic-hk) ; Vedic Harvard-Kyoto -> IAST
+(require 'pie) ; Proto-Indo-European
+(require 'glagolitic) ; Old Church Slavonic Glagolitic Script
+(require 'avestan-trans) ; Avestan Transcription
+(require 'avestan-script) ; Avestan Script
+(require 'old-persian) ; Old Persian Cuneiform
+(require 'oscan) ; Oscan Script
+(require 'gothic) ; Gothic Script
 
 (provide 'pie-macs)
